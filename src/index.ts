0241f7f283024370a8380f369fbd2a0734febf00
scapestudio0@gmail.com <scapestudio0@gmail.com>
2022-01-27 14:49:50 -0500

    fix: code clean up

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { ethers } from "hardhat";
 
-ethers.provider.on("block", async () => {
-  //   console.log(await ethers.provider.getBlock("latest"));
+ethers.provider.on("block", async block => {
+  const blockNumber = await ethers.provider.getBlock("latest");
+  // console.log(await ethers.provider.getBlock("latest"));
   const uniswapPairAddr = "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852";
   const uniswapPair = await ethers.getContractAt(
     ["function getReserves() view returns (uint112, uint112, uint32)"],
     uniswapPairAddr,
   );
   const reserves = await uniswapPair.getReserves();
-  console.log(`${reserves[0].toString()} ----- ${reserves[1].toString()}`);
+  console.log(
+    `\nBLOCK # ${
+      blockNumber.number
+    }\n  UniSwap Pair Reserves:\n\t ${reserves[0].toString()} ----- ${reserves[1].toString()}\n`,
+  );
 
   const sushiSwapPairAddr = "0x06da0fd433C1A5d7a4faa01111c044910A184553";
   const sushiSwapPair = await ethers.getContractAt(
@@ -16,5 +21,7 @@ ethers.provider.on("block", async () => {
     sushiSwapPairAddr,
   );
   const reservesSushiSwap = await sushiSwapPair.getReserves();
-  console.log(`${reservesSushiSwap[0].toString()} ---- ${reservesSushiSwap[1].toString()}`);
+  console.log(
+    `  SushiSwap Pair Reverves:\n\t ${reservesSushiSwap[0].toString()} ---- ${reservesSushiSwap[1].toString()}\n===================================================`,
+  );
 });
